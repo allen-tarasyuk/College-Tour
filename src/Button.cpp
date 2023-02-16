e5484d5f5127d539f20ff163a68927d0ea2b3e3c
bryceberwald <bryce@berwald.io>
2023-02-16 08:53:02 -0800

    Added different button background colors depending on if the cursor is over it...

--- src/Button.cpp
+++ src/Button.cpp
@@ -8,15 +8,14 @@ Button::~Button(){
 
 }
 
-void Button::CreateButton(int width, int height, int x, int y){
+void Button::CreateButton(int width, int height, int x, int y, Color color){
     this->width = width;
     this->height = height;
     this->x = x;
     this->y = y;
 
-    DrawRectangle(width, height, x, y, GRAY);
-    DrawRectangleLines(width, height, x, y, BLACK);
-    
+    DrawRectangle(width, height, x, y, color);
+    Rectangle tempRect = {(float)width, (float)height, (float)x, (float)y};
+    DrawRectangleLinesEx(tempRect, 4.0f, BLACK);
 }
-
 
--- src/main.cpp
+++ src/main.cpp
@@ -24,13 +24,46 @@ int main(void)
             ClearBackground(WHITE);
             DrawText("Closet College to other college locations", 130, 30, 24, BLACK);
             
+            /* SADDLEBACK BUTTON BACKGROUND COLOR LOGIC */
             Button SaddlebackBtn;
-            SaddlebackBtn.CreateButton(20, 100, 300, 70);
+            if((GetMouseX() >= 20 && GetMouseX() <= 320) && (GetMouseY() >= 100 && GetMouseY() <= 170)){
+                SaddlebackBtn.CreateButton(20, 100, 300, 70, GRAY);
+            } else {
+                SaddlebackBtn.CreateButton(20, 100, 300, 70, DARKGRAY);
+            }
             DrawText((char*)"Saddleback", TextLength((char*)"Saddleback") / 2 + 120, 125, 20, WHITE);
-            
+
+            /* ASU BUTTON BACKGROUND COLOR LOGIC */
             Button ASU_Btn;
-            ASU_Btn.CreateButton(400, 100, 300, 70);
+            if((GetMouseX() >= 460 && GetMouseX() <= 700) && (GetMouseY() >= 100 && GetMouseY() <= 170)){
+                ASU_Btn.CreateButton(400, 100, 300, 70, GRAY);
+            } else {
+                ASU_Btn.CreateButton(400, 100, 300, 70, DARKGRAY);
+            }
             DrawText((char*)"ASU", TextLength((char*)"ASU") / 2 + 520, 125, 20, WHITE);
+
+            /* UCI BUTTON BACKGROUND COLOR LOGIC */
+            Button UCI_Btn;
+            if((GetMouseX() >= 20 && GetMouseX() <= 320) && (GetMouseY() >= 200 && GetMouseY() <= 270)){
+                UCI_Btn.CreateButton(20, 200, 300, 70, GRAY);
+            } else {
+                UCI_Btn.CreateButton(20, 200, 300, 70, DARKGRAY);
+            }
+            DrawText((char*)"UCI", (TextLength((char*)"UCI") / 2) + 150, 225, 20, WHITE);
+            
+            /* CUSTOM TRIP BUTTON BACKGROUND COLOR LOGIC */
+            Button CustomTripBtn;
+            if((GetMouseX() >= 420 && GetMouseX() <= 720) && (GetMouseY() >= 200 && GetMouseY() <= 270)){
+                CustomTripBtn.CreateButton(400, 200, 300, 70, GRAY);
+            } else {
+                CustomTripBtn.CreateButton(400, 200, 300, 70, DARKGRAY);
+            }
+            DrawText((char*)"Custom Trip", (TextLength((char*)"Custom Trip") / 2) + 475, 225, 20, WHITE);
+
+            /* DRAW CONTENT TO BE DISPLAYED AREA ... */
+            DrawRectangle(20, 300, 680, 450, LIGHTGRAY);
+            DrawRectangleLines(20, 300, 680, 450, BLACK);
+            
 
         EndDrawing();
         //----------------------------------------------------------------------------------
